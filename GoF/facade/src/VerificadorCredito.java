1efcdfbba561597865570ee16c93b390b447a4dc
6r1nCHy <84735143+6r1nchy@users.noreply.github.com>
2025-06-19 03:20:48 -0300

    :sparkles: Adicionado uma implementação de facade com Java puro.

---
+++ GoF/facade/src/VerificadorCredito.java
@@ -0,0 +1,6 @@
+public class VerificadorCredito {
+    public boolean temBomHistorico(String cliente) {
+        System.out.println("Verificando histórico de crédito do cliente: " + cliente);
+        return true;
+    }
+}
